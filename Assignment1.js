bf1e84efe701782d1e395e76f2b394b0520e092d
Brian Adelt <ba8621@mcla.edu>
2019-02-03 18:43:17 +0000

    Assignment1

--- Assignment1.js
+++ Assignment1.js
@@ -36,9 +36,6 @@ function requestHandler(req, res)
     console.log(query);
     var word1 = query["word1"];
     var word2 = query["word2"];
-    var comb = word1.concat(word2);
-    var Len = comb.length;
-    //var Len2=word2.length;
    var x = '';
     for (var i = 0; i < (30 - (word1.length + word2.length)); i++)
     {
@@ -47,9 +44,35 @@ function requestHandler(req, res)
     res.write('<pre>'+word1+x+word2+'</pre>');
       res.end('');    
     }
+ 
+   if (query['cmd'] == 'stats')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var grades = query['grades'];
+      var input = [grades];
+      var sum = 0;
+      for (var i in query['grades'])
+      {
+        sum = sum + parseInt(query['grades'][i]);
+      }
+      
+      var GradeAvg = sum/query['grades'].length;
+      
+      const GradesMax = function(input)
+     {
+       return Math.max(...input);
+     };
+     const GradesMin = function(input)
+     {
+        return Math.min(...input);
+     };
+     
+      res.write('<pre>'+"Avg: "+GradeAvg+" Min: "+GradesMin(...input)+" Max: "+GradesMax(...input)+'</pre>');
+      res.end('');
+    }
     else
     {
       res.end('');
     }
-
 }
